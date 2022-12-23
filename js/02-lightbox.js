8cbdef4a848ca4b1e0286a449517bfc01721a347
Mariia1811 <115475304+Mariia1811@users.noreply.github.com>
2022-12-23 21:04:29 +0200

    rework

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,25 +1,20 @@
 import { galleryItems } from "./gallery-items.js";
 
 const listRef = document.querySelector(".gallery");
-listRef.addEventListener("click", onImgClick);
 
-const createImgEl = ({ preview, original, description }) => {
-  return `
+const createImgEl = galleryItems
+  .map(({ preview, original, description }) => {
+    return `
   <a class="gallery__item" href="${original}">
   <img class="gallery__image" src="${preview}" alt="${description}" />
 </a>
     `;
-};
+  })
+  .join("");
 
-const elements = galleryItems.map(createImgEl).join("");
-listRef.insertAdjacentHTML("beforeend", elements);
+listRef.insertAdjacentHTML("beforeend", createImgEl);
 
-function onImgClick(e) {
-  e.preventDefault();
-  if (e.target.classList.contains("gallery__image")) {
-    let lightbox = new SimpleLightbox(".gallery a", {
-      captionsData: "alt",
-      captionDelay: 250,
-    });
-  }
-}
+let lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
