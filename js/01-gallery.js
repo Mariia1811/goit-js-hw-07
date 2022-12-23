84ae525025ed794ce04becd9ae1e10f2909450fb
Mariia1811 <115475304+Mariia1811@users.noreply.github.com>
2022-12-23 20:26:49 +0200

    add completed tasks

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,53 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const divRef = document.querySelector(".gallery");
 
-console.log(galleryItems);
+const createImgEl = galleryItems
+  .map(({ preview, original, description }) => {
+    return `
+    <div class="gallery__item">
+    <a class="gallery__link" href="${original}">
+      <img
+        class="gallery__image"
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"
+        />
+    </a>
+  </div>
+    `;
+  })
+  .join("");
+
+divRef.insertAdjacentHTML("beforeend", createImgEl);
+
+divRef.addEventListener("click", onImgClick);
+
+function onImgClick(e) {
+  e.preventDefault();
+
+  if (!e.target.classList.contains("gallery__image")) {
+    return;
+  }
+
+  const instance = basicLightbox.create(
+    `
+        <img src=${e.target.dataset.source} width="800" height="600">
+    `,
+    {
+      onShow: (instance) => {
+        window.addEventListener("keydown", onKeydownEacape);
+      },
+      onClose: (instance) => {
+        window.removeEventListener("keydown", onKeydownEacape);
+      },
+    }
+  );
+  instance.show();
+
+  function onKeydownEacape(e) {
+    if (e.code === "Escape") {
+      instance.close();
+    }
+  }
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,25 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
 
-console.log(galleryItems);
+const listRef = document.querySelector(".gallery");
+listRef.addEventListener("click", onImgClick);
+
+const createImgEl = ({ preview, original, description }) => {
+  return `
+  <a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>
+    `;
+};
+
+const elements = galleryItems.map(createImgEl).join("");
+listRef.insertAdjacentHTML("beforeend", elements);
+
+function onImgClick(e) {
+  e.preventDefault();
+  if (e.target.classList.contains("gallery__image")) {
+    let lightbox = new SimpleLightbox(".gallery a", {
+      captionsData: "alt",
+      captionDelay: 250,
+    });
+  }
+}
